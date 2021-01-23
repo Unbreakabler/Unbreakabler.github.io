60763df648cc1a887b334bba7ad51936a5ceaa4d
Unbreakabler <jonboyd21@gmail.com>
2021-01-22 16:59:48 -0800

    remove all

--- src/app/app.svelte
+++
@@ -1,8 +0,0 @@
-
-<script>
-  import AddTwoNumbers from './add-two-numbers.svelte'
-</script>
-
-<h1>esbuild svelte plugin example - test</h1>
-
-<AddTwoNumbers />
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import App from './app/app.svelte'
-
-new App({
-  target: document.body
-})
